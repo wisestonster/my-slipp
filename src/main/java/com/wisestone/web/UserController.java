1a4917f7c9a207894701e12e94da513c42758af4
DESKTOP-F72FJ8C\wises <wisestonster@gmail.com>
2020-09-14 22:56:54 +0900

    3-3 강의까지 반영

--- src/main/java/com/wisestone/web/UserController.java
+++ src/main/java/com/wisestone/web/UserController.java
@@ -1,18 +1,17 @@
 package com.wisestone.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.wisestone.domain.User;
 import com.wisestone.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 	
 //	private List<User> users = new ArrayList<User>();
@@ -20,23 +19,18 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
-	@GetMapping("/")
-	public String index() {
-		return "index";
-	}
-	
-	@PostMapping("/create")
+	@PostMapping("")
 	public String create(User user) {
 		System.out.println("userId: " + user.toString());
 //		users.add(user);
 		userRepository.save(user);
-		return "redirect:/list";
+		return "redirect:/users";
 	}
 	
-	@GetMapping("/list")
+	@GetMapping("")
 	public String list(Model model) {
 //		model.addAttribute("users", users);
 		model.addAttribute("users", userRepository.findAll());
-		return "list";
+		return "/user/list";
 	}
 }
