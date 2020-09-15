90e4954f1a977bdfa033633589a10af30aef3a04
DESKTOP-2B0SMLO\RB <wisestonster@gmail.com>
2020-09-15 16:14:02 +0900

    3-4. 개인정보 수정 기능 구현 1

--- src/main/java/com/wisestone/web/UserController.java
+++ src/main/java/com/wisestone/web/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,6 +20,12 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@GetMapping("/form")
+	public String form() {
+
+		return "/user/form";
+	}
+	
 	@PostMapping("")
 	public String create(User user) {
 		System.out.println("userId: " + user.toString());
@@ -33,4 +40,16 @@ public class UserController {
 		model.addAttribute("users", userRepository.findAll());
 		return "/user/list";
 	}
+
+	@GetMapping("/{id}/form")
+	public String updateForm(@PathVariable Long id, Model model) {
+
+		User user = userRepository.findById(id).get();  
+		model.addAttribute("user", user); 
+		
+//		model.addAttribute("user", userRepository.findOne(id));
+		
+		return "/user/updateForm";
+	}
+
 }
