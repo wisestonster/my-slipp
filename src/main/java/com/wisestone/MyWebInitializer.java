0eabd9cabd21ddf7f0efc58df5e6ca65648964ba
DESKTOP-F72FJ8C\wises <wisestonster@gmail.com>
2020-09-18 12:29:22 +0900

    외부 tomcat 사용가능하도록 pom 파일 수정, SpringBootServletInitializer 추가

---
+++ src/main/java/com/wisestone/MyWebInitializer.java
@@ -0,0 +1,12 @@
+package com.wisestone;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class MyWebInitializer extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(MySlippApplication.class);
+	}
+}
