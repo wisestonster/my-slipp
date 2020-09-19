2d97bae3fdf6fb788fa75fe9fd0df3e4a34b8fcd
DESKTOP-F72FJ8C\wises <wisestonster@gmail.com>
2020-09-19 23:03:31 +0900

    질문 상세보기 기능까지 구현

--- src/main/java/com/wisestone/web/QuestionController.java
+++ src/main/java/com/wisestone/web/QuestionController.java
@@ -4,7 +4,9 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -47,6 +49,13 @@ public class QuestionController {
 		}
 		
 		return "/qna/form";
+	}
+	
+	@GetMapping("/{id}")
+	public String show(@PathVariable Long id, Model model) {
+		model.addAttribute("question", questionRepository.findById(id).get());
+		
+		return "/qna/show";	
 	}
 
 }
